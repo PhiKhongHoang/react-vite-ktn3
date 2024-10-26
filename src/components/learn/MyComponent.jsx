817b799b6d11d7f0a337f6b42583ded2d8090f68
Phi Khong Hoang <phihoangkhong@gmail.com>
2024-10-27 01:51:50 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,13 +4,27 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric"; // string
+    // const hoidanit = 18; // number   
+    // const hoidanit = true; // boolean      
+    // const hoidanit = undefined; // undefined  
+    // const hoidanit = null; // null  
+    const hoidanit = [1, 2, 3]; // array  
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 18
+    // }; // object  
+
     return (
         <>
-            <div>eric & hoidanit update</div>
+            <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+            <div>{console.log(">>>> eric")}</div>
             <div className="child"
                 style={
                     { borderRadius: "10px" }
-                }>child</div>
+                }>
+                child
+            </div>
         </>
     );
 }
