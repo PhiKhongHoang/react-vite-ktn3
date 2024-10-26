ffee08269f9d06f33c8800c7adc1838fe2e632f6
Phi Khong Hoang <phihoangkhong@gmail.com>
2024-10-27 01:39:29 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 // () => {}
 //component = html + css + js
 
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div>eric & hoidanit update</div>
+        <>
+            <div>eric & hoidanit update</div>
+            <div className="child"
+                style={
+                    { borderRadius: "10px" }
+                }>child</div>
+        </>
     );
 }
 
